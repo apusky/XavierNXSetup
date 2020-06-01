dd5338233b292154706921e02342a7755b2fa94d
Luis Chachayma <chachayma.luis@gmail.com>
2020-06-01 12:00:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ echo "---install jupyter lab service"
 cd ~/AI/XavierNXSetup
 echo $password | sudo jupyter lab -y --generate-config
 echo $password | sudo python3 set_jupyter_password.py $password
-echo $password | sudo -S python3 create_jupyter_service.py
+python3 create_jupyter_service.py #it need to be executed to create the service for the current user
 echo $password | sudo mv xaviernx_jupyter.service /etc/systemd/system/xaviernx_jupyter.service
 sudo systemctl enable xaviernx_jupyter
 sudo systemctl start xaviernx_jupyter
