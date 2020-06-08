033eb31cb3198a1b99f34a05153614aea4576420
Luis Chachayma <chachayma.luis@gmail.com>
2020-06-08 13:45:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -86,7 +86,7 @@ echo "---install tensorflow"
 echo $password | sudo -S apt-get -y install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
 echo $password | sudo pip3 install -U pip testresources setuptools
 echo $password | sudo pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
-echo $password | sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 ‘tensorflow<2’
+echo $password | sudo pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow==1.15.2+nv20.4
 #echo $password | sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v44 tensorflow
 
 #install pytorch
